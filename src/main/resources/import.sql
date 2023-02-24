221f646ac1034537661f5cbe96f5aa2b30320d8b
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2023-02-24 19:43:59 -0300

    role update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO tb_role(authority) VALUES ('MEMBER');
-INSERT INTO tb_role(authority) VALUES ('VISITOR');
+INSERT INTO tb_role(authority) VALUES ('ROLE_MEMBER');
+INSERT INTO tb_role(authority) VALUES ('ROLE_VISITOR');
 
 INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Ana', 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
